99f6d3bfae5668b7c300de3bc8054866090e6957
Ayush Singhal <ayush.singhal.as30@gmail.com>
2022-05-05 00:38:02 +0530

    Created sample app

---
+++ Danger/app/src/main/java/developer/ayush/danger/MainActivity.kt
@@ -0,0 +1,11 @@
+package developer.ayush.danger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
