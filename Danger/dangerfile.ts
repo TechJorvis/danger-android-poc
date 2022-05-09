5d5677c0aa01788e9b2d6d9af8369726b24940a0
Ayush Singhal <ayush.singhal.as30@gmail.com>
2022-05-09 11:56:59 +0530

    Update dangerfile.ts

--- Danger/dangerfile.ts
+++ Danger/dangerfile.ts
@@ -3,7 +3,7 @@ import {message, danger} from "danger"
 const newFiles = danger.git.created_files;
 
 if (newFiles && newFiles.length > 0) {
-    message("New Files in this PR: \n - " + newFiles..join("- "));
+    message("New Files in this PR: \n - " + newFiles.join("- "));
 } else {
     message("No New Files in this PR: \n");   
 }
