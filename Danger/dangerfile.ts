9a3b8cbff97ad06e2a00654e2310c296c29bb585
Ayush Singhal <ayush.singhal.as30@gmail.com>
2022-05-09 19:10:58 +0530

    Update dangerfile.ts

--- Danger/dangerfile.ts
+++ Danger/dangerfile.ts
@@ -5,22 +5,26 @@ const modifiedFiles = danger.git.modified_files;
 const deletedFiles = danger.git.deleted_files;
 
 if (newFiles && newFiles.length > 0) {
-    message("New Files in this PR: \n - " + newFiles.join("- "));
+    message("New Files in this PR: \n" + newFiles.join("- "));
 } else {
     message("No New Files in this PR.\n");   
 }
 
 if (modifiedFiles && modifiedFiles.length > 0) {
+    message("Modified Files in this PR: \n" + modifiedFiles.join("- "));
+    
     modifiedFiles.forEach(value => {
-        message(value + "\n");
+        message("=> File:" + value + "\n");
     });
 } else {
     message("No files were modified in this PR.");
 }
 
 if (deletedFiles && deletedFiles.length > 0) {
+    message("Deleted Files in this PR: \n" + modifiedFiles.join("- "));
+
     deletedFiles.forEach(value => {
-        message(value + "\n");
+        message("=> File:" + value + "\n");
     });
 } else {
     message("No files were deleted in this PR.");
