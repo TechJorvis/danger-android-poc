7ecf91bef11f5acce7101203d33d7bca5037d71c
Ayush Singhal <ayush.singhal.as30@gmail.com>
2022-05-09 11:55:11 +0530

    fixed dangerfile

--- Danger/dangerfile.ts
+++ Danger/dangerfile.ts
@@ -1,5 +1,10 @@
 import {message, danger} from "danger"
 
-const newFiles = danger.git.created_files.join("- ");
+const newFiles = danger.git.created_files;
 
-message("New Files in this PR: \n - " + newFiles);
+if (newFiles && newFiles.length > 0) {
+    message("New Files in this PR: \n - " + newFiles..join("- "));
+} else {
+    message("No New Files in this PR: \n");   
+}
+
