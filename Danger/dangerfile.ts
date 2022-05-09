620d9317d97142e928d255e55671ba08911c7971
Ayush Singhal <ayush.singhal.as30@gmail.com>
2022-05-10 00:44:47 +0530

    Update dangerfile.ts

--- Danger/dangerfile.ts
+++ Danger/dangerfile.ts
@@ -1,31 +1,46 @@
-import {message, danger} from "danger"
+import { NoNewFileDanger } from "./danger-rules/danger/message/NoNewFileDanger"
+import { IncorrectPRTitleDanger } from "./danger-rules/danger/warn/IncorrectPRTitle";
 
-const newFiles = danger.git.created_files;
-const modifiedFiles = danger.git.modified_files;
-const deletedFiles = danger.git.deleted_files;
+new NoNewFileDanger()
+    .checkAndNotify("No New Files in this PR.");
 
-if (newFiles && newFiles.length > 0) {
-    message("New Files in this PR: \n" + newFiles.join("- "));
-} else {
-    message("No New Files in this PR.\n");   
-}
+new IncorrectPRTitleDanger()
+    .checkAndNotify("Incorrect PR Title. PR Title should be in the format => \"[Ticket-ID] [Type] Title\"")
 
-if (modifiedFiles && modifiedFiles.length > 0) {
-    // message("Modified Files in this PR: \n" + modifiedFiles.join("- "));
+
+
+
+
+
+
+// import {message, danger} from "danger"
+
+// const newFiles = danger.git.created_files;
+// const modifiedFiles = danger.git.modified_files;
+// const deletedFiles = danger.git.deleted_files;
+
+// if (newFiles && newFiles.length > 0) {
+//     message("New Files in this PR: \n" + newFiles.join("- "));
+// } else {
+//     message("No New Files in this PR.\n");   
+// }
+
+// if (modifiedFiles && modifiedFiles.length > 0) {
+//     // message("Modified Files in this PR: \n" + modifiedFiles.join("- "));
     
-    modifiedFiles.forEach(value => {
-        message("Modified File: " + value + "\n");
-    });
-} else {
-    message("No files were modified in this PR.");
-}
+//     modifiedFiles.forEach(value => {
+//         message("Modified File: " + value + "\n");
+//     });
+// } else {
+//     message("No files were modified in this PR.");
+// }
 
-if (deletedFiles && deletedFiles.length > 0) {
-    // message("Deleted Files in this PR: \n" + modifiedFiles.join("- "));
+// if (deletedFiles && deletedFiles.length > 0) {
+//     // message("Deleted Files in this PR: \n" + modifiedFiles.join("- "));
 
-    deletedFiles.forEach(value => {
-        message("Deleted File: " + value + "\n");
-    });
-} else {
-    message("No files were deleted in this PR.");
-}
+//     deletedFiles.forEach(value => {
+//         message("Deleted File: " + value + "\n");
+//     });
+// } else {
+//     message("No files were deleted in this PR.");
+// }
