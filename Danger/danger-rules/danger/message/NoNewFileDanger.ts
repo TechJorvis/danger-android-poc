88677ab1f694d0f0a0ceff7ad901d33f3b849f95
Ayush Singhal <ayush.singhal.as30@gmail.com>
2022-05-10 01:08:22 +0530

    Update NoNewFileDanger.ts

--- Danger/danger-rules/danger/message/NoNewFileDanger.ts
+++ Danger/danger-rules/danger/message/NoNewFileDanger.ts
@@ -3,7 +3,7 @@ import { DangerType } from "../../constants/DangerType";
 import { MessageNotificationChannel } from "../../danger_notifier/MessageNotificationChannel";
 import { Danger } from "../Danger";
 
-export default class NoNewFileDanger extends Danger {
+class NoNewFileDanger extends Danger {
 
     readonly msg: string = "No New Files in this PR."
 
@@ -22,3 +22,5 @@ export default class NoNewFileDanger extends Danger {
         return isDanger;
     }
 }
+
+module.exports = NoNewFileDanger;
