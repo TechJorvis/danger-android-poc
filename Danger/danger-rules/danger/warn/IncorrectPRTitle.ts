f9ca11516cf1c022b31bf4e01ee3740305b8806e
Ayush Singhal <ayush.singhal.as30@gmail.com>
2022-05-10 00:44:43 +0530

    Create IncorrectPRTitle.ts

---
+++ Danger/danger-rules/danger/warn/IncorrectPRTitle.ts
@@ -0,0 +1,21 @@
+import { danger } from "danger";
+import { DangerType } from "../../constants/DangerType";
+import { WarningNotificationChannel } from "../../danger_notifier/WarningNotificationChannel";
+import { Danger } from "../Danger";
+
+export class IncorrectPRTitleDanger extends Danger {
+
+    private static PR_TITLE_REGEX = /(\[[A-Za-z]+-[0-9]+\])\s(\[[A-Za-z]+\])\s([A-Za-z0-9]+)$/g;
+
+    constructor() {
+        super(DangerType.Warn, new WarningNotificationChannel())
+    }
+
+    protected isDanger(): boolean {
+        const prTitle = danger.github.pr.title;
+        
+        const isCorrect = IncorrectPRTitleDanger.PR_TITLE_REGEX.test(prTitle);
+
+        return !isCorrect;
+    }
+}
